d7b0bb24c5683ee9039bea7e4875d9751e5e481d
stefaniftime <iftime_stefan99@yahoo.com>
2024-05-19 19:54:38 +0300

    add sql command to ingest data into mysql database through command line

---
+++ dataset/Import from CSV to MYSQL.sql
@@ -0,0 +1,1 @@
+LOAD DATA INFILE "C:/Users/Edy/project-web/web/dataset/result/merged_file.csv" INTO TABLE cars_dataset FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
